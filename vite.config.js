3d0599b502de4ea55c6dc9adb91f8d2ce75e4675
Nikita_28 <160863172+Nikitaa0115@users.noreply.github.com>
2025-04-11 19:15:12 +0530

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,10 @@
+export default {
+  server: {
+    host: '0.0.0.0',
+    port: parseInt(process.env.PORT) || 4173
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: parseInt(process.env.PORT) || 4173
+  }
+}
