8a9ef41c6f035f47f459cca2c8b1d8030b0bb41c
Nikita_28 <160863172+Nikitaa0115@users.noreply.github.com>
2025-04-26 18:57:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const sampleData = require("./models/Sample.js");
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = 8080;
 
 
 // ✅ Allowed origins including deployed frontend
