113c2c2a8830d35dcca99955e805af5fe0b1cfc0
Nikita_28 <160863172+Nikitaa0115@users.noreply.github.com>
2025-05-18 19:52:49 +0530

    Update ContactUs.jsx

--- FRONTEND/src/Components/ContactUs.jsx
+++ FRONTEND/src/Components/ContactUs.jsx
@@ -19,7 +19,7 @@ const ContactUs = () => {
     e.preventDefault();
 
     try {
-      const response = await axios.post("https://train-depot.onrender.com/api/contact", formData);
+      const response = await axios.post("https://train-depot.onrender.com", formData);
 
 
 
